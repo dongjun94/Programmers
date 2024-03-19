eb6be46c99f1d4a45ba9df599ba69356d2b7872e
dongjun94 <wlsleh12@gmail.com>
2024-03-19 14:38:23 +0900

    add solve

---
+++ level2/84512.cpp
@@ -0,0 +1,39 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+#include <string>
+#include <vector>
+#include <map>
+#include <math.h>
+
+using namespace std;
+#define endl "\n"
+
+map<char, int> m_index = {{'A', 1}, {'E', 2}, {'I', 3}, {'O', 4}, {'U', 5}, };
+
+vector<int> vec_weight;
+void make_weight()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        vec_weight.emplace_back(0);
+        for (int j = 4 - i; j >= 0; j--)
+            vec_weight.back() += pow(5, j);
+    }
+}
+
+int solution(string word)
+{
+    make_weight();
+
+    int answer = word.size();
+    for (int i = 0; i < word.size(); i++)
+        answer += vec_weight.at(i) * (m_index.at(word.at(i)) - 1);
+
+    return answer;
+}
+
+int main()
+{
+    solution(string("EIO"));
+    return 0;
+}
